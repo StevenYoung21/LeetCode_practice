df59fec6b52d999499a42ae89bb22c0fc28c9837
StevenYoung21 <719192406@qq.com>
2020-05-12 23:49:55 +0800

    283-移动零

---
+++ arry/283-moveZeroes/1.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+
+    let nums0 = 0;
+    let len = nums.length;
+
+    for(let i = 0; i < len ; i++){
+        if (nums[i] == 0){
+            nums.splice(i,1);
+            nums0++;
+        }
+    }
+
+    // while( nums0 > 0){
+    //     nums.push(0);
+    //     nums0--;
+    // }
+
+    return nums;
+};
+
+var arr = [0,0,1];
+console.log(moveZeroes(arr));
