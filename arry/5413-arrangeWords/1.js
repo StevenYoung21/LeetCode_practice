1f0bdbe0d6702b1458a5de0da6933f3953ed8a57
StevenYoung21 <719192406@qq.com>
2020-05-17 12:11:20 +0800

    5413-重排单词重新排列句子中的单词

---
+++ arry/5413-arrangeWords/1.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} text
+ * @return {string}
+ */
+var arrangeWords = function(text) {
+
+    let arr = [];
+    arr = text.toLocaleLowerCase().split(' ');
+
+    let len = arr.map(e=>e.length);
+    let temp = '';
+    let tempLen = 0;
+    
+    for(let i = 0; i < arr.length - 1 ; i++){  // 冒泡排序
+        for(let j = 0; j < arr.length - i -1 ; j++){
+
+            if (len[j] > len[j + 1]) {
+                 temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                tempLen = len[j];
+                len[j] = len[j+1];
+                len[j+1] = tempLen;
+            }
+        }
+        
+    }
+    let endArr = [];
+    endArr = arr.join(' ');
+    
+
+    return endArr.charAt(0).toUpperCase() + endArr.slice(1);  // 将字符串首字母大写
+
+}
+
+var text = "To be or not to be";
+console.log(arrangeWords(text));
