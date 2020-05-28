e174c68df7764626f6dfcd0fed2543736cf09d4c
StevenYoung21 <719192406@qq.com>
2020-05-28 17:52:46 +0800

    557-反转字符串中的单词 III

---
+++ string/557-reverseWords/1.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+
+    s = s.split('').reverse().join('').split(' ').reverse().join(' ')
+
+   return s;
+
+};
+
+var s = "Let's take LeetCode contest";
+console.log(reverseWords(s));
